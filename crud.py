35ae1761976df5b8a2805e6860ec2dc9dabcf99c
Yan Ribeiro Nunes <yrn@cesar.school>
2024-11-19 22:56:42 -0300

    Update crud.py

--- crud.py
+++ crud.py
@@ -2,3 +2,14 @@ import os
 
 def limpar_tela():
     os.system("cls" if os.name == "nt" else "clear")
+    
+#Commit 12: Função definir_meta (Parte 2)
+    while True:
+        try:
+            valor = float(input("Qual o valor da meta (ex.: 100 km): "))
+            if valor <= 0:
+                raise ValueError("O valor da meta deve ser positivo.")
+            break
+        except ValueError as e:
+            print(f"Erro: {e}. Tente novamente.")
+
