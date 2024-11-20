83ced91488c32336919517c75720089af02f5716
João Victor Uchôa <jvgcu@cesar.school>
2024-11-19 23:13:08 -0300

    Update crud.py

--- crud.py
+++ crud.py
@@ -15,6 +15,12 @@ def inicializar_arquivos():
     if not os.path.exists(METAS_FILENAME):
         with open(METAS_FILENAME, mode='w', encoding='utf-8') as file:
             file.write("Meta|Valor|Progresso\n")
+#Commit 4: Função adicionar_registro (Parte 1)
+
+def adicionar_registro():
+    limpar_tela()
+    print("=== Adicionar Novo Treino ou Competição ===")
+    data = input("Data (DD-MM-AAAA): ")
     
 #Commit 12: Função definir_meta (Parte 2)
     while True:
